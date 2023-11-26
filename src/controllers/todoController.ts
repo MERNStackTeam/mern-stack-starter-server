f8b4e524dcd6e3ec3d8d7d91fbe2a28992d0468a
Jinto Antony <jinto1729@gmail.com>
2023-11-26 17:28:36 +0530

    Todo - Route and Controller separated

---
+++ src/controllers/todoController.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from 'express';
+import Todo from '../database/schemas/Todo';
+import { ErrorRequestHandler } from 'express';
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next: NextFunction) => {
+    res.status(500).json({ message: (err as Error).message });
+};
+
+export const getAllTodos = async (req: Request, res: Response) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+    } catch (err: any) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export const createTodo = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { title, description } = req.body;
+        // Perform validation if needed
+
+        const newTodo = await Todo.create({ title, description });
+        res.status(201).json(newTodo);
+    } catch (err) {
+        errorHandler(err, req, res, next);
+    }
+};
+
+export const updateTodo = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const { title, description } = req.body;
+        // Perform validation if needed
+
+        const updatedTodo = await Todo.findByIdAndUpdate(id, { title, description }, { new: true });
+        if (!updatedTodo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+        res.json(updatedTodo);
+    } catch (err) {
+        errorHandler(err, req, res, next);
+    }
+};
